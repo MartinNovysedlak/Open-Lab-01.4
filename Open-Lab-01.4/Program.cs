8990f3c41b1116bcbf6ea118676701c4b565f9d0
MartinNovysedlak <martinnovysedlak48@gmail.com>
2019-10-09 21:32:26 +0200

    Initial

---
+++ Open-Lab-01.4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Open_Lab_01._4
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int s = Triangel(4, 2);
+            Console.WriteLine(s);
+        }
+        public static int Triangel(int a, int b)
+        {
+            return a * b / 2;
+        }
+    }
+}
